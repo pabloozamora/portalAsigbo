811f7627c4cd7952c44b207697677d4567f42582
Erick Guerra <erickguerra2201@gmail.com>
2023-10-01 01:58:46 -0600

    Corrección en importar usuarios

--- apiServices/user/user.model.js
+++ apiServices/user/user.model.js
@@ -424,7 +424,7 @@ const uploadUsers = async ({ users, session }) => {
   } catch (ex) {
     if (ex.errors) throw new CustomError(ex.errors[Object.keys(ex.errors)].message, 400);
     if (ex.code === 11000) {
-      if (ex.message.includes('code')) throw new CustomError(`El id ${ex.writeErrors[0].err.op.code} ya existe en la base de datos`, 400);
+      if (ex.message.includes('code')) throw new CustomError(`El código ${ex.writeErrors[0].err.op.code} ya existe en la base de datos`, 400);
       throw new CustomError(`El correo ${ex.writeErrors[0].err.op.email} ya existe en la base de datos`, 400);
     }
     throw ex;
