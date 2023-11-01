81df760827c39459569b35a3b912fa3f9f37dbee
Diego <diegoguatedb2002@gmail.com>
2023-11-01 13:41:53 -0600

    fix: response error por limite de img size

--- middlewares/multerMiddleware.js
+++ middlewares/multerMiddleware.js
@@ -3,11 +3,15 @@ export default (multerInstance) => (req, res, next) => {
     if (!err) next();
     else {
       let error = err?.message ?? 'Ocurrió un error al subir imagen.';
+      let status = err?.status ?? 500;
 
-      if (err?.code === 'LIMIT_FILE_SIZE') error = 'El tamaño del archivo es demasiado grande. El tamaño máximo es de 1 MB.';
+      if (err?.code === 'LIMIT_FILE_SIZE') {
+        error = 'El tamaño del archivo es demasiado grande. El tamaño máximo es de 1 MB.';
+        status = 413;
+      }
 
       res.statusMessage = error;
-      res.sendStatus(err?.status ?? 500);
+      res.status(status).send({ err: error, status });
     }
   });
 };
